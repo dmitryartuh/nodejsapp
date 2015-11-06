a6ebddd286e15f2d5b318b869aab79a726a63dd9
Dmitry Artux <d.artuh@colours.nl>
2015-11-06 16:25:56 +0300

    settings

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by dya on 11/6/2015.
  */
+var express = require('express');
+var io = require('socket.io');
+
+var app = express()
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+    res.status = 200;
+    res.send('hello world')
+});
+
+app.listen(3000);
