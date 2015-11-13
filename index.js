625233ad55e1dabbecf0fd264eb5b127edea6fe2
dima <dima@gmail.com>
2015-11-14 00:56:00 +0300

    dev

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
  */
 var express = require('express');
 var io = require('socket.io');
+var engine = require('ejs-locals');
+
 
 var app = express()
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-    res.status = 200;
-    res.send('hello world')
+    res.sendfile('views/layout.html', {})
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 13.11.2015.
+ */
+var loginApp = angular.module('myApp', []);
+
+loginApp.controller('login_form', function(){
+
+    this.login = "zxvxzvc";
+    this.pass = "";
+});
