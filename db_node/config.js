fa9136abcb9fa5c05f4e3a7da03906c2a78fe2a8
dima <dima@gmail.com>
2015-11-23 21:57:36 +0300

    dev

--- db_node/config.js
+++ db_node/config.js
@@ -3,6 +3,9 @@
  */
 var config = {};
 config.start_port = '4001';
-config.couchdb_address = 'http://localhost:5984';
+config.couchdb_address = 'http://192.168.137.250:5984';
+config.db_name = {
+    users: 'cz_users'
+};
 
 module.exports = config;
--- db_node/index.js
+++ db_node/index.js
@@ -3,12 +3,14 @@
 // */
 var config = require('./config')
 var express = require('express');
+var util = require('util');
+var async = require('async');
 var app = express();
 var server = app.listen(config.start_port);
 var io = require('socket.io').listen(server);
 
 var nano = require('nano')(config.couchdb_address);
-var db_name = "test";
+var db_name = config.db_name.users;
 var db = nano.use(db_name);
 
 io.sockets.on('connection', function (socket) {
@@ -17,9 +19,12 @@ io.sockets.on('connection', function (socket) {
 	console.log('received data: ' + data.login + ', ' + data.pass);
         if(data.login != undefined && data.login != null &&
             data.pass != undefined && data.pass != null){
-				nano.db.use('test').list(function(err, body) {
+				nano.db.use(db_name).list(function(err, body) {
 					if (!err) {
+                        var count = 0;
+                        var finished = false;
 						body.rows.forEach(function(doc) {
+                            count ++;
 							db.get(doc.id, function(err, doc) {
                                 if(doc.login != undefined && doc.pass != undefined){
                                     if(doc.login == data.login && doc.pass == data.pass){
@@ -27,13 +32,20 @@ io.sockets.on('connection', function (socket) {
                                             result: doc._id
                                         });
 										console.log('found id = ' + doc._id);
-										return;
+                                        finished = true;
                                     }
+                                }
+                                if(count == body.rows.length && !finished){
+                                    console.log('not found');
+                                    socket.emit('send_checked_data', {
+                                        result: null
+                                    });
                                 }
 							});
 						});
 					}
                     else{
+						console.log('get user list error')
                         socket.emit('send_checked_data', {
                             result: null
                         });
@@ -41,6 +53,7 @@ io.sockets.on('connection', function (socket) {
 				});
 		}
         else{
+            console.log('input data is not valid')
             socket.emit('send_checked_data', {
                 result: null
             });
@@ -49,7 +62,18 @@ io.sockets.on('connection', function (socket) {
 });
 console.log('started');
 
-
+nano.db.use(db_name).list(function(err, body) {
+    if (!err) {
+        body.rows.forEach(function(doc) {
+            db.get(doc.id, function(err, doc) {
+                console.log(util.inspect(doc));
+            });
+        });
+    }
+    else{
+        console.log('start get users error');
+    }
+});
 
 
 
