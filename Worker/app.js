f20ead35ff862bffd18dc8a77ad5089b97be2fcd
dima <dima@gmail.com>
2015-12-07 22:54:31 +0300

    dev

--- Worker/app.js
+++ Worker/app.js
@@ -21,17 +21,24 @@ app.get('/isWorking', function(req, res){
 });
 
 io.sockets.on('connection', function (socket) {
-  console.log('Client connected... ' + socket.id);
-  manager.add_user(uf.create_user(socket.id, socket.handshake.query.login));
+    console.log('Client connected... ' + socket.id);
+    var user = manager.find_user(socket.id);
+    if(user == null){
+        manager.add_user(uf.create_user(socket.id, socket));
+    }
+    else{
+        socket.emit('double_connect');
+        return;
+    }
 
   socket.on('new_game', function(){
     async.waterfall([
         function(callback){
-            callback(manager.find_user(socket.id));
+            callback(null, manager.find_user(socket.id));
         },
         function(user, callback){
           if(user != null){
-              callback(user, gs.find_wait_game());
+              callback(null, user, manager.find_wait_game());
           }
         },
         function(user, game){
@@ -39,14 +46,16 @@ io.sockets.on('connection', function (socket) {
               game.add_user(user.id);
               user.game_session = game.id;
               socket.join('room' + game.id);
-              io.to('room' + game.id).emit('game_started', game);
+              io.to('room' + game.id).emit('started_new_game', game);
               console.log('started game ' + game.id + ' with ' + game.users_id[0] + ' and ' + user.id);
           }
           else{
               var number = get_room_number();
               manager.start_new_game(gf.create_new_game_session(number, user.id));
               user.game_session = number;
+              socket.join('room' + number);
               console.log('created game ' + number + ' with ' + user.id);
+              socket.emit('wait_partner', manager.find_game(number));
           }
         }], function(err){
             console.log('error with new_game');
@@ -57,11 +66,34 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function(){
         console.log('Client disconnected... ' + socket.id);
+      manager.remove_user(socket.id);
   });
 
-  //socket.on('update_state', function(){
-  //
-  //});
+  socket.on('update_state', function(data){
+      var game = manager.find_game(data.id);
+      var set_step_res = game.set_step(data);
+      if(set_step_res){
+          var step_res = game.next_step();
+          if(!step_res){
+                var winner_id = game.get_winner();
+                var losser_id = game.get_losser();
+              console.log('winner - ' + winner_id);
+              console.log('losser - ' + losser_id);
+              var winner = manager.find_user(winner_id);
+              var losser = manager.find_user(losser_id);
+              winner.socket.emit('win');
+              losser.socket.emit('lost');
+          }
+          else{
+              io.to('room' + game.id).emit('update', game);
+          }
+      }
+      else{
+          console.log('set next step erro');
+          socket.emit('error');
+      }
+
+  });
   //
   //socket.on('game_finished', function(){
   //
--- app/public/js/game.js
+++ app/public/js/game.js
@@ -5,14 +5,11 @@
 gameApp.controller('gameController', ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout){
     $scope.game_started = false;
     this.start_game = function() {
-        if(this.socket != undefined && this.socket.connected) {
-            this.socket.disconnect();
+		var gc = this;
+        if(this.socket === undefined) {
+            this.socket = io.connect($rootScope.worker_url);
         }
-        this.socket = io.connect($rootScope.worker_url);
-
-
-
-
+        
         this.socket.on('connect_failed', function () {
 
         });
@@ -20,18 +17,85 @@ gameApp.controller('gameController', ['$scope', '$rootScope', '$timeout', functi
         this.socket.on('connecting', function () {
             console.log('connecting...');
         });
+		
+		this.socket.on('wait_partner', function(data) {
+			console.log(data);
+		});
+		
+		this.socket.on('double_connect', function(data){
+			console.log('double_connect');
+		});
+		
+		this.socket.on('won', function(data){
+			$scope.game_started = false;
+			$scope.$apply();
+			console.log('won');
+		});
+		
+		this.socket.on('lost', function(data){
+			$scope.game_started = false;
+			$scope.$apply();
+			console.log('lost');
+		});
 
-        this.socket.on('connect', function () {
+        this.socket.on('started_new_game', function (game) {
             $scope.game_started = true;
+			gc.game = game;
+			gc.step = false;
+			if(gc.game.current_user == gc.socket.id){
+				gc.step = true;
+			}
             $scope.$apply();
             $timeout(function() {
                 var cw = $('.image').width();
                 $('.image').css({'height':cw+'px'});
             }, 100);
         });
-
-        this.setZero = function (i, j) {
-            $('#' + i + j + '.image').removeClass('empty').addClass('cross');
-        }
-    }
+		
+		this.socket.on('update', function(game) {
+			if(game.current_user == gc.socket.id){
+				gc.step = true;
+			}
+			gc.updateState(game.state);
+		});
+		
+		this.socket.emit('new_game');
+    };
+	
+	
+	this.setZero = function (i, j) {
+		if(this.step && $scope.game_started) {
+			if(this.game.state[i - 1][j - 1] != -1){
+				return;
+			}
+			this.step = false;
+			this.socket.emit('update_state', {
+				id: this.game.id,
+				x: i - 1,
+				y: j - 1
+			});
+		}
+		// $('#' + i + j + '.image').removeClass().addClass(this.step);
+	};
+	
+	this.updateState = function(sessionState){
+		if(sessionState != null){
+			for (var i = 0; i < 3; i++) {
+				for (var j = 0; j < 3; j++){
+					var ii = i + 1;
+					var jj = j + 1;
+					$('#' + ii + jj + '.image').removeClass('zero cross empty');
+					if(sessionState[i][j] == 0){
+						$('#' + ii + jj + '.image').addClass('zero');
+					}
+					else if (sessionState[i][j] == 1){
+						$('#' + ii + jj + '.image').addClass('cross');
+					}
+					else {
+						$('#' + ii + jj + '.image').addClass('empty');
+					}					
+				}
+			}
+		}
+	};
 }]);
